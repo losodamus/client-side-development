dec206f24d67786d15a74a8bde9ab6178fc52f32
Charles II <cjlakes@yahoo.com>
2022-08-28 08:00:47 -0700

    Update toPrototypeDateString.js

--- toPrototypeDateString.js
+++ toPrototypeDateString.js
@@ -2,13 +2,13 @@
     Simple JavaScript example of Date prototyping.
     Output as a [monthAbbr]. [day], [year] format.
 
-    console.log((new Date()).toCustomDateString());
+    console.log((new Date()).toPrototypeDateString());
     >>> August 19, 2022
 
-    console.log((new Date(2004, 3)).toCustomDateString());
+    console.log((new Date(2004, 3)).toPrototypeDateString());
     >>> April 1, 2004
 
-    console.log((new Date(2004, 3, 15)).toCustomDateString());
+    console.log((new Date(2004, 3, 15)).toPrototypeDateString());
     >>> April 15, 2004
 */
 Date.prototype.toPrototypeDateString = function() {
